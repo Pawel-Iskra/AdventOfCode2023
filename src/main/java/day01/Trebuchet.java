c398caf586d8916fd3b6d1194187fc9c3e05fb01
Pawel <paweliszcz@wp.pl>
2023-12-02 20:04:24 +0100

    [AoC2023] Day01

---
+++ src/main/java/day01/Trebuchet.java
@@ -0,0 +1,28 @@
+package day01;
+
+import utils.MyUtilities;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Trebuchet {
+
+    private static String partOne(List<String> inputLines) {
+
+        return "";
+    }
+
+    private static String partTwo(List<String> inputLines) {
+
+        return "";
+    }
+
+    public static void main(String[] args) throws IOException {
+        String pathToInputFile = "src/main/resources/day01/input.txt";
+        List<String> inputLines = MyUtilities.getInputLines(pathToInputFile);
+
+
+        System.out.println("Part 1 = " + partOne(inputLines));
+        System.out.println("Part 2 = " + partTwo(inputLines));
+    }
+}
