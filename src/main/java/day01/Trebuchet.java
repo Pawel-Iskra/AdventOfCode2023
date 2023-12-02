e46799907f930ef23a9ca213cef062d10ca91072
Pawel <paweliszcz@wp.pl>
2023-12-02 20:22:53 +0100

    [AoC2023] Day01 solutions

--- src/main/java/day01/Trebuchet.java
+++ src/main/java/day01/Trebuchet.java
@@ -7,12 +7,42 @@ import java.util.List;
 
 public class Trebuchet {
 
-    private static String partOne(List<String> inputLines) {
+    private static List<String> stringDigitList = List.of("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
 
-        return "";
+    private static String findFirstDigit(final String line) {
+
+        return null;
+    }
+
+    private static String findLastDigit(final String line) {
+        return null;
+    }
+
+    private static String partOne(List<String> inputLines) {
+        int[] sumOfCalibrationValues = new int[1];
+        inputLines.forEach(line -> {
+            String onlyDigitsString = line.replaceAll("[^\\d.]", "");
+            String firstDigit = String.valueOf(onlyDigitsString.charAt(0));
+            String lastDigit = String.valueOf(onlyDigitsString.charAt(onlyDigitsString.length() - 1));
+            int calibrationValue = Integer.parseInt(firstDigit + lastDigit);
+            sumOfCalibrationValues[0] += calibrationValue;
+        });
+
+        return String.valueOf(sumOfCalibrationValues[0]);
     }
 
     private static String partTwo(List<String> inputLines) {
+        int[] sumOfCalibrationValues = new int[1];
+        inputLines.forEach(line -> {
+            String firstDigit = findFirstDigit(line);
+            String lastDigit = findLastDigit(line);
+            int length = line.length();
+
+
+
+            int calibrationValue = Integer.parseInt(firstDigit + lastDigit);
+            sumOfCalibrationValues[0] += calibrationValue;
+        });
 
         return "";
     }
