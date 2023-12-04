857aaa96f6c433f036292fe92870065b83c9f480
Pawel <paweliszcz@wp.pl>
2023-12-04 18:24:57 +0100

    [AoC2023] Day04 part1 changes + part2

--- src/main/java/day04/Scratchcards.java
+++ src/main/java/day04/Scratchcards.java
@@ -9,70 +9,91 @@ import java.util.List;
 
 public class Scratchcards {
 
-    private static long partOne(List<String> inputLines) {
-        long result = 0;
-        List<List<Integer>> myNumbersList = new ArrayList<>();
-        List<List<Integer>> winningNumbersList = new ArrayList<>();
 
+    private static List<List<Integer>> myNumbersListOfLists = new ArrayList<>();
+    private static List<List<Integer>> winningNumbersListOfLists = new ArrayList<>();
+    private static int amountOfCards = 0;
+
+    private static void generateCardsBeforePlay(List<String> inputLines) {
         inputLines.forEach(line -> {
-            String[] winningNumStringArray = line.substring(line.indexOf(":") + 1 , line.indexOf("|")).strip().split(" ");
+            String[] winningNumStringArray = line.substring(line.indexOf(":") + 1, line.indexOf("|")).strip().split(" ");
             String[] myNumbersStringArray = line.substring(line.indexOf("|") + 1).strip().split(" ");
-            System.out.println("Winnnings:");
-            Arrays.stream(winningNumStringArray).forEach(num -> System.out.println(num));
-            System.out.println("\nMy nums:");
-            Arrays.stream(myNumbersStringArray).forEach(num -> System.out.println(num));
-//            System.out.println("winningNumString = " + winningNumString);
-//            System.out.println("myNumbers = " + myNumbersString);
 
             List<Integer> current = new ArrayList<>();
             final List<Integer> finalCurrent = current;
             Arrays.stream(winningNumStringArray).filter(number -> !number.isBlank()).forEach(num -> {
                 finalCurrent.add(Integer.parseInt(num.strip()));
             });
-            winningNumbersList.add(current);
+            winningNumbersListOfLists.add(current);
             current = new ArrayList<>();
             final List<Integer> finalCurrent1 = current;
             Arrays.stream(myNumbersStringArray).filter(number -> !number.isBlank()).forEach(num -> {
                 finalCurrent1.add(Integer.parseInt(num.strip()));
             });
-            myNumbersList.add(finalCurrent1);
-            System.out.println("winningNumbersList = " + winningNumbersList);
-            System.out.println("myNumbersList = " + myNumbersList);
+            myNumbersListOfLists.add(finalCurrent1);
+//            System.out.println("winningNumbersListOfLists = " + winningNumbersListOfLists);
+//            System.out.println("myNumbersListOfLists = " + myNumbersListOfLists);
         });
-        int numOfSeries = winningNumbersList.size();
-        for(int i = 0; i < numOfSeries; i++){
-            List<Integer> currentWinnings = winningNumbersList.get(i);
-            List<Integer> currentMyNums = myNumbersList.get(i);
-            System.out.println("currentWinnings = " + currentWinnings);
-            System.out.println("currentMyNums = " + currentMyNums);
+        amountOfCards = winningNumbersListOfLists.size();
+    }
+
+    private static long partOne() {
+        long result = 0;
+
+        for (int i = 0; i < amountOfCards; i++) {
+            List<Integer> currentWinnings = winningNumbersListOfLists.get(i);
+            List<Integer> currentMyNums = myNumbersListOfLists.get(i);
+//            System.out.println("currentWinnings = " + currentWinnings);
+//            System.out.println("currentMyNums = " + currentMyNums);
             long currentResult = 0;
             for (Integer currentWinningNum : currentWinnings) {
-                if(currentMyNums.contains(currentWinningNum)){
-
+                if (currentMyNums.contains(currentWinningNum)) {
                     if (currentResult == 0) currentResult = 1;
-                    else currentResult *=2;
+                    else currentResult *= 2;
                 }
             }
-            System.out.println("currentResult = " + currentResult);
-            result +=currentResult;
-
+//            System.out.println("currentResult = " + currentResult);
+            result += currentResult;
         }
-
         return result;
     }
 
-    private static String partTwo(List<String> inputLines) {
+    private static int partTwo() {
+        int[] numbersOfCardsInstances = new int[amountOfCards];
+        for (int i = 0; i < amountOfCards; i++) {
+            numbersOfCardsInstances[i] = 1;
+        }
 
-        return "";
+        for (int i = 0; i < amountOfCards; i++) {
+            List<Integer> currentWinnings = winningNumbersListOfLists.get(i);
+            List<Integer> currentMyNums = myNumbersListOfLists.get(i);
+            int counter = 0;
+            int currentCardInstances = numbersOfCardsInstances[i];
+            for (Integer currentWinningNum : currentWinnings) {
+                if (currentMyNums.contains(currentWinningNum)) {
+                    counter++;
+                }
+            }
+
+            for (int z = 0; z < currentCardInstances; z++) {
+                for (int j = 0; j < counter; j++) {
+                    numbersOfCardsInstances[i + 1 + j]++;
+                }
+            }
+
+        }
+//        for (int i = 0; i < amountOfCards; i++) {
+//            System.out.println(i + " -> " + numbersOfCardsInstances[i]);
+//        }
+        return Arrays.stream(numbersOfCardsInstances).sum();
     }
 
     public static void main(String[] args) throws IOException {
         String pathToInputFile = "src/main/resources/day04/input.txt";
         List<String> inputLines = MyUtilities.getInputLines(pathToInputFile);
-        inputLines.forEach(line -> System.out.println(line));
+        generateCardsBeforePlay(inputLines);
 
-        System.out.println("\nPART I = " + partOne(inputLines));
-        System.out.println("PART II = " + partTwo(inputLines));
-
+        System.out.println("\nPART I = " + partOne());
+        System.out.println("PART II = " + partTwo());
     }
 }
